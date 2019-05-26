14b23d4afbe575c164889284179b22760da9d1f1
thinkpaduser <vladlonglake@gmail.com>
2019-05-26 21:06:05 +0300

    Accidently pushed the wrong file

--- git-bootstrap.sh
+++ git-bootstrap.sh
@@ -2,8 +2,8 @@
 id=$(id -u)
 key='your id_rsa.pub contents'
 if [ $id -ne 0 ]; then
-       	echo "This script requires root privileges! Exiting... "
-       	exit 1
+        echo "This script requires root privileges! Exiting... "
+        exit 1
 fi
 echo "Your id is $id"
 echo;
@@ -12,10 +12,9 @@ pkg_add git
 echo "Adding a group git if not exists"
 [ $(getent group git) ] || groupadd git
 echo "Adding a user git if not exists"
-[ $(getent passwd git) ] || useradd git -g git
+[ $(getent passwd git) ] || useradd -G git git
 mkdir -p /home/git/test-repo.git 2> /dev/null
-echo $key > /home/git/.ssh/authorized_keys
+mkdir -p /home/git/.ssh/
+echo $key >> /home/git/.ssh/authorized_keys
 chown git:git /home/git/test-repo.git
-cd /home/git/test-repo.git
-git init --bare
-echo "Done."
+
